002533e9a26f00d3ef053daae652670dbd25ec17
LivKrogshede <74859654+LivKrogshede@users.noreply.github.com>
2021-03-02 09:40:25 +0100

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,20 @@
+function filterObjects(category){ 
+    //find all boxes
+    let allBoxes = document.querySelectorAll('.objects .box');
+    // show all if cateogry is 'all'
+    if(category == "all"){
+        allBoxes.forEach(function(el){ //The forEach() method calls a function once for each element in an array, in order.
+            el.style.display = 'block';
+        });
+    } else {
+        // Else show category only.
+        allBoxes.forEach(function(el){
+            el.style.display = 'none';
+        });
+
+        let categoryBoxes = document.querySelectorAll('.objects .' + category);
+        categoryBoxes.forEach(function(el){
+            el.style.display = 'block'; 
+        });
+    }
+}
