a8b602748e5ec6dde7681180aa4f6a61c9fc23ce
LivKrogshede <74859654+LivKrogshede@users.noreply.github.com>
2021-03-04 10:39:51 +0100

    done done

--- core.js
+++ core.js
@@ -1,18 +1,17 @@
-function myFunction() { //Lavet en funktion der sætter gang i processen
-  var input, filter, ul, li, a, i, txtValue; //Får lavet og navngivet variablerne
-  input = document.getElementById('searchBox'); //Giver variablerne værdi
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  ul.style.display = "block";
-  li = ul.getElementsByTagName('li');
+function myFunction() {
+   var input, filter, ul, li, a, i, txtValue;
+   input = document.getElementById('search');
+   filter = input.value.toUpperCase();
+   ul = document.getElementById("myUL");
+   li = ul.getElementsByTagName('li');
 
-  for (i = 0; i < li.length; i++) { //påbegynder koden der fær searchbox til at virke
-    a = li[i].getElementsByTagName('a')[0]; //Tager fat i 'a' tagget
-    txtValue = a.textContent || a.innerText; //Sørger for at den kommer ind på HTML
-    if (txtValue.toUpperCase().indexOf(filter) > -1) { //Hvis tekst er mere end 0 og matcher med de lister jeg har lavet
-      li[i].style.display = ""; //Listen er synlig
-    } else {
-      li[i].style.display = "none"; //Hvis mindre end 1 så usynlig
-    }
-  }
+   for (i = 0; i < li.length; i++) {
+     a = li[i].getElementsByTagName('a')[0];
+     txtValue = a.textContent || a.innerText;
+     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+       li[i].style.display = "";
+     } else {
+       li[i].style.display = "none";
+     }
+   }
 }
