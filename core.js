097e884e4d91f7d41f504bac3f1653dbc51d06a6
Sej1999 <74958302+Sej1999@users.noreply.github.com>
2021-03-04 09:46:43 +0100

    Updated searchbar

--- core.js
+++ core.js
@@ -1,17 +1,18 @@
 function myFunction() {
-   var input, filter, ul, li, a, i, txtValue;
-   input = document.getElementById('search');
-   filter = input.value.toUpperCase();
-   ul = document.getElementById("myUL");
-   li = ul.getElementsByTagName('li');
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchBox');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  ul.style.display = "block";
+  li = ul.getElementsByTagName('li');
 
-   for (i = 0; i < li.length; i++) {
-     a = li[i].getElementsByTagName('a')[0];
-     txtValue = a.textContent || a.innerText;
-     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-       li[i].style.display = "";
-     } else {
-       li[i].style.display = "none";
-     }
-   }
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName('a')[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
