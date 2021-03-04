a025877284c2fea7ceec9a6830c3eebd98805fed
Sej1999 <74958302+Sej1999@users.noreply.github.com>
2021-03-04 12:42:51 +0100

    Chat-funktion index done

---
+++ chat.js
@@ -0,0 +1,10 @@
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  /*
+The code is inspired by w3school; https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup_chat
+*/
