b39be652f159bfa967286b4be0423d57cfff8ddd
MissDuvier <73283644+MissDuvier@users.noreply.github.com>
2021-03-04 10:33:51 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -5,3 +5,6 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
+  /*
+The code is inspired by w3school; https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup_chat
+*/
